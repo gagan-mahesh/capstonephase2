5546ad4f542b9a19b05f796ea6a2356a6c9f9cd8
gagan-mahesh <gaganmaheshhebbar@outlook.com>
2021-06-10 09:09:39 +0530

    removed celery .

--- Capstone_Phase2/app.py
+++ Capstone_Phase2/app.py
@@ -4,69 +4,16 @@ sys.path.insert(1, '../')
 from flask import Flask,render_template, redirect, url_for
 from news_demo import *
 from Capstone_Phase2.tweepySample import * 
-from celery import Celery
 
 app = Flask(__name__)
-app.config['CELERY_BROKER_URL'] = 'amqp://gagan:gagan123@localhost/gagan_vhost'
-app.config['CELERY_RESULT_BACKEND'] = 'amqp://gagan:gagan123@localhost/gagan_vhost'
-
-celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
-celery.conf.update(app.config)
-
-@celery.task(bind=True)
-def summariseBackground(self):
-	print("SUMMARISING TEXT...")
-	summary = temp()
-	self.update_state(state='PROGRESS', meta={'list:', summary})
-	# print("TEXT SUMMARISED!!!\n\n\n")
-	# print(summary, "\n\n")
-	time.sleep(1)
-	# j = {'list': summary}
-	return {'list': summary}
 
 @app.route("/")
 def hello():
 	return "<h1>Front Page with product title</h1>"
 
-@app.route('/temp')
-def temporary():
-	task = summariseBackground.apply_async()
-	return redirect(url_for('printHyperlinks', task_id=task.id))
-
-# task id is required by celery inorder to update the messaging queue...
-@app.route('/printHyperlinks/<task_id>')
-def printHyperlinks(task_id):
-	task = summariseBackground.AsyncResult(task_id)
-	if task.state == 'PENDING':
-		print("\n\nPENDING....\n\n")
-		response = {
-			'state': task.state,
-			'list': None
-		}
-	elif task.state == 'FAILURE':
-		print("\n\nFAILURE...\n\n")
-		response = {
-			'state': task.state,
-			'list': "failure",
-		}
-	else:
-		response = {
-			'state': task.state,
-			'list': task.info
-		}
-
-	return response, 200
-
 @app.route("/summary")
 def summarized_output():
 	summary = get_summary()
-	# summary = summariseBackground.delay()
-	# print("SUMMARIZATION FINISHED...? ", summary.ready())
-	# print("SUMMARIZATION RESULT ", summary.result)
-	# time.sleep(3)
-	# print("SUMMARIZATION FINISHED...? ", summary.ready())
-	# print("SUMMARIZATION RESULT ", summary.result)
-	# return "", 200
 	summary_json = {}
 	summarized_output = []
 	for i in summary.result:
