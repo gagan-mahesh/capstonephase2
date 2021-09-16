81b45fad708c04d9f5a82c2c032b5674d40766a1
mohantyabhijit074 <mohantyabhijit074@gmail.com>
2021-09-16 20:55:20 +0530

    added emoji to text

--- Capstone_Phase2/tweepyFood.py
+++ Capstone_Phase2/tweepyFood.py
@@ -2,6 +2,8 @@ import tweepy
 import csv
 import sys, time
 import json
+import demoji
+demoji.download_codes()
 sys.path.insert(1, '../')
 from emotion import *
 
@@ -13,6 +15,18 @@ access_token_secret = 'g9zcBGTLPl8L7ZqkwxkbwlDaG7SOPUgUXgXuRFn0JBbkd'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+
+def emojis_to_text(text):
+	emojies=demoji.findall(text)
+	i=0
+	for char in text:
+	    if char in emojies:
+	        text=text[:i]+emojies[char]+" "+text[i+1:]
+	        i+=len(emojies[char])+1
+	    else:
+	        i+=1
+
+	return text
 
 def get_reviews(hashtag,num_of_tweets):
 	all_reviews = []
@@ -31,6 +45,7 @@ def get_reviews(hashtag,num_of_tweets):
 				res+=i+str(" ")
 
 		if len(res) > 10:
+			res = emojis_to_text(res)
 			counter+=1 
 			all_reviews.append(res)
 		counter+=1
